f79c505acbfde6be3690d2e719129f7f9abff99e
Arpit Verma <99536186+arpitvermaofficial@users.noreply.github.com>
2022-10-13 00:47:41 +0530

    Added proper documentation

--- flutter project/Text to speech convertor/utils.dart
+++ flutter project/Text to speech convertor/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Command{
+class Command{ //Commands which perform if text contain these commands words
   static final all=[email,browser1,browser2];
   static const email="write email";
   static const browser1="open";
@@ -12,23 +12,23 @@ class Command{
 class Utils{
   static void scanText(String rawtext){
     final text=rawtext.toLowerCase();
-    if(text.contains(Command.email)){
-      final body=_getTextAfterCommand(Text:text,command:Command.email);
+    if(text.contains(Command.email)){ //check if text conatin write email word
+      final body=_getTextAfterCommand(Text:text,command:Command.email); //check the next words after command word
       openemail(body:body);
-    }else if(text.contains(Command.browser1)){
+    }else if(text.contains(Command.browser1)){ //check if text conatin open word
       final url=_getTextAfterCommand(Text:text,command:Command.browser1);
       openLink(Url:url);
     }
-    else if(text.contains(Command.phone)){
+    else if(text.contains(Command.phone)){ // check if text contain call to word
       final url=_getTextAfterCommand(Text:text,command:Command.phone);
 
       call(Url:url);
-    }else if(text.contains(Command.sms)){
+    }else if(text.contains(Command.sms)){ //check if text contain sms to word
       final url=_getTextAfterCommand(Text:text,command:Command.sms);
       sms(Url:url);
     }
   }
-  static _getTextAfterCommand({
+  static _getTextAfterCommand({ //scan the next word after command word
     required String Text,required String command
 }){
     final indexcommand=Text.indexOf(command);
@@ -39,7 +39,7 @@ class Utils{
       return Text.substring(inddexafter).trim();
     }
   }
-  static Future openemail({required String body})async{
+  static Future openemail({required String body})async{ //open the email 
     final Uri params = Uri(
         scheme: 'mailto',
         path: 'arpitv747@gmail.com',
@@ -52,7 +52,7 @@ class Utils{
     }
 
   }
-  static Future openLink({required String Url})async{
+  static Future openLink({required String Url})async{ 
     final Uri _url1 = Uri.parse('https://www.google.co.in/');
     final Uri _url2=Uri.parse('https://$Url');
     if(Url.trim().isEmpty){
@@ -61,9 +61,9 @@ class Utils{
       await launchUrl(_url2);
     }
   }
-  static Future call({required String Url})async{
+  static Future call({required String Url})async{ //open the phone app and dial 807*****51
     final Uri _url = Uri(scheme: 'tel',path:Url);
-    final Uri _url1 = Uri(scheme: 'tel',path:'8077485551');
+    final Uri _url1 = Uri(scheme: 'tel',path:'807*****51');
 
     if(Url.trim().isEmpty){
       await launchUrl(_url1);
@@ -71,7 +71,7 @@ class Utils{
       launchUrl(_url);
     }
   }
-  static Future sms({required String Url})async{
+  static Future sms({required String Url})async{  //open the sms app and dial 807*****51
     final Uri _url = Uri(scheme: 'sms',path:Url);
     final Uri _url1 = Uri(scheme: 'sms',path:'8077485551');
 
